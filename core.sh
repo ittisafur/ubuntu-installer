2f1226b85b2f033bbc176fc825f8584db940545c
Ittisafur <ittisafur@gmail.com>
2023-08-17 22:16:59 -0700

    Update Core

--- core.sh
+++ core.sh
@@ -2,7 +2,8 @@
 
 # Check if all the required packages are installed, and if not, install them
 core() {
-# List of required packages
+    echo "Checking if core packages are installed..."
+
     local packages=("curl" "wget" "git" "htop" "tree" "vim" "meson" "ninja" "cmake" "gcc" "g++" "make" "fzf")
     
     # Assume all packages are installed initially
@@ -20,19 +21,21 @@ core() {
     if [ "$all_installed" = true ]; then
         echo "All core packages are already installed."
     else
-        echo "Installing core packages..."
-        apt update
-        apt upgrade -y
-        echo "Installing Ubuntu Restricted Extras, Steam, Build Essentials, Curl, Wget, Git, Htop, Tree, Vim, Meson, Ninja, CMake, GCC, G++, Make, FZF, APT Transport HTTPS, CA Certificates, GNUPG..."
-        apt install ubuntu-restricted-extras steam-installer build-essential curl wget git htop tree vim meson ninja-build cmake gcc g++ make fzf apt-transport-https ca-certificates gnupg -y
-        echo "Installing ZSH..."
-        apt install zsh -y
-        echo "Changing default shell to ZSH..."
-        chsh -s $(which zsh)
-        echo "Installing ZAP..."
-        zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
-        echo "Done installing core packages."
+      install_core
     fi
 }
 
-echo "Installation completed."
+install_core(){
+    echo "Installing core packages..."
+    apt update
+    apt upgrade -y
+    echo "Installing Ubuntu Restricted Extras, Steam, Build Essentials, Curl, Wget, Git, Htop, Tree, Vim, Meson, Ninja, CMake, GCC, G++, Make, FZF, APT Transport HTTPS, CA Certificates, GNUPG..."
+    apt install ubuntu-restricted-extras steam-installer build-essential curl wget git htop tree vim meson ninja-build cmake gcc g++ make fzf apt-transport-https ca-certificates gnupg -y
+    echo "Installing ZSH..."
+    apt install zsh -y
+    echo "Changing default shell to ZSH..."
+    chsh -s $(which zsh)
+    echo "Installing ZAP..."
+    zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+    echo "Done installing core packages."
+}
